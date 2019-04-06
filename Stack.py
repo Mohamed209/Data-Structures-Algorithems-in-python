9eff94819597f3183b387783efd37371ee9ef5cd
Mohamed209 <mohamedmosad209@gmail.com>
2019-04-06 02:46:21 +0200

    check for balanced Parentheses using stack

--- Stack.py
+++ Stack.py
@@ -36,3 +36,18 @@ def reverse(string):
         stack.pop()
     return srev
 print(reverse('hello'))
+
+# check for balanced Parentheses using stack
+def isBalanced(exp):
+    if len(exp)%2!=0 or len(exp)==0:
+        return False
+    else:
+        s=Stack()
+        for ch in exp:
+            if ch in '(':
+                s.push(ch)
+            elif ch in ')':
+                if s.isEmpty():return False
+                else :s.pop()
+        return s.isEmpty()
+print(isBalanced('))(('))
